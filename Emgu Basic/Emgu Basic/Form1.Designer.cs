6544ba52165c34e74ccd01513eaf0ab031420e7a
Wai-Kei Law <w.k.law@students.uu.nl>
2022-10-11 12:42:20 +0200

    Initial commit

---
+++ Emgu Basic/Emgu Basic/Form1.Designer.cs
@@ -0,0 +1,122 @@
+﻿
+namespace Emgu_Basic
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.LoadBTN = new System.Windows.Forms.Button();
+            this.pictureBox = new System.Windows.Forms.PictureBox();
+            this.pedestrianCheck = new System.Windows.Forms.CheckBox();
+            this.yoloBox = new System.Windows.Forms.CheckBox();
+            this.timeLBL = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // LoadBTN
+            // 
+            this.LoadBTN.Location = new System.Drawing.Point(611, 11);
+            this.LoadBTN.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.LoadBTN.Name = "LoadBTN";
+            this.LoadBTN.Size = new System.Drawing.Size(191, 47);
+            this.LoadBTN.TabIndex = 0;
+            this.LoadBTN.Text = "Load Image AND Detect";
+            this.LoadBTN.UseVisualStyleBackColor = true;
+            this.LoadBTN.Click += new System.EventHandler(this.LoadBTN_Click);
+            // 
+            // pictureBox
+            // 
+            this.pictureBox.Location = new System.Drawing.Point(12, 63);
+            this.pictureBox.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.pictureBox.Name = "pictureBox";
+            this.pictureBox.Size = new System.Drawing.Size(790, 457);
+            this.pictureBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox.TabIndex = 1;
+            this.pictureBox.TabStop = false;
+            // 
+            // pedestrianCheck
+            // 
+            this.pedestrianCheck.AutoSize = true;
+            this.pedestrianCheck.Location = new System.Drawing.Point(12, 12);
+            this.pedestrianCheck.Name = "pedestrianCheck";
+            this.pedestrianCheck.Size = new System.Drawing.Size(94, 20);
+            this.pedestrianCheck.TabIndex = 2;
+            this.pedestrianCheck.Text = "Pedestrian";
+            this.pedestrianCheck.UseVisualStyleBackColor = true;
+            this.pedestrianCheck.CheckedChanged += new System.EventHandler(this.pedestrianCheck_CheckedChanged);
+            // 
+            // yoloBox
+            // 
+            this.yoloBox.AutoSize = true;
+            this.yoloBox.Checked = true;
+            this.yoloBox.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.yoloBox.Location = new System.Drawing.Point(12, 38);
+            this.yoloBox.Name = "yoloBox";
+            this.yoloBox.Size = new System.Drawing.Size(124, 20);
+            this.yoloBox.TabIndex = 3;
+            this.yoloBox.Text = "Yolo Dectection";
+            this.yoloBox.UseVisualStyleBackColor = true;
+            this.yoloBox.CheckedChanged += new System.EventHandler(this.yoloBox_CheckedChanged);
+            // 
+            // timeLBL
+            // 
+            this.timeLBL.AutoSize = true;
+            this.timeLBL.Location = new System.Drawing.Point(9, 533);
+            this.timeLBL.Name = "timeLBL";
+            this.timeLBL.Size = new System.Drawing.Size(88, 16);
+            this.timeLBL.TabIndex = 5;
+            this.timeLBL.Text = "Load Image...";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(814, 562);
+            this.Controls.Add(this.timeLBL);
+            this.Controls.Add(this.yoloBox);
+            this.Controls.Add(this.pedestrianCheck);
+            this.Controls.Add(this.pictureBox);
+            this.Controls.Add(this.LoadBTN);
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button LoadBTN;
+        private System.Windows.Forms.PictureBox pictureBox;
+        private System.Windows.Forms.CheckBox pedestrianCheck;
+        private System.Windows.Forms.CheckBox yoloBox;
+        private System.Windows.Forms.Label timeLBL;
+    }
+}
+
---
+++ Emgu Basic/Emgu Basic/Form1.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+using System.Diagnostics;
+using Emgu.CV;
+using Emgu.CV.Structure;
+using Emgu.CV.Cuda;
+using Emgu.CV.Models;
+using Accord.Video.FFMPEG;
+using System.Threading.Tasks;
+using System.IO;
+
+
+
+namespace Emgu_Basic
+{
+    public partial class Form1 : Form
+    {
+        Image<Bgr, byte> image;
+        CudaImage<Bgr, byte> image2;
+        Yolo yolo = new Yolo();
+        VideoFileReader vFReader = new VideoFileReader();
+        PedestrianDetector detector = new PedestrianDetector();
+        bool video = false;
+        System.Windows.Forms.Timer timer1 = new Timer();
+        Stopwatch stopwatch = new Stopwatch();
+        int frameElappsed = 0;
+        bool pedestianDetection = false;
+
+        public Form1()
+        {
+            InitializeComponent();
+            Debug.Write(CudaInvoke.HasCuda.ToString());
+            init();
+        }
+
+        private async void init()
+        {
+            await yolo.Init(Yolo.YoloVersion.YoloV4Tiny, null);
+            await detector.Init();
+        }
+
+        private void videoDetection()
+        {
+            stopwatch = Stopwatch.StartNew();
+            timer1.Interval = 10;
+            timer1.Tick += yoloDectionTimer;
+            timer1.Start();
+            if(frameElappsed >= vFReader.FrameCount)
+            {
+                stop();
+            }
+        }
+
+        private void stop()
+        {
+            timer1.Stop();
+            stopwatch.Stop();
+            vFReader.Close();
+            timeLBL.Text = "Done in " + stopwatch.ElapsedMilliseconds + " ms";
+        }
+
+       
+        private void yoloDectionTimer(object sender, EventArgs myEventArgs)
+        {
+            Bitmap bmpBaseOriginal = vFReader.ReadVideoFrame();
+            if(bmpBaseOriginal != null)
+            {
+                image = bmpBaseOriginal.ToImage<Bgr, byte>();
+                string text = yolo.ProcessAndRender(image, image);
+                timeLBL.Text = text;
+                byte[] bytes = image.ToJpegData();
+                Image x = (Bitmap)((new ImageConverter()).ConvertFrom(bytes));
+                pictureBox.Image = x;
+                frameElappsed++;
+            }
+        }
+
+        private void yoloDection()
+        {
+            string text = yolo.ProcessAndRender(image2, image);
+            timeLBL.Text = text;
+            byte[] bytes = image.ToJpegData();
+            Image x = (Bitmap)((new ImageConverter()).ConvertFrom(bytes));
+            pictureBox.Image = x;
+        }
+
+        private void detectPersons()
+        {
+            string text = detector.ProcessAndRender(image2, image);
+            timeLBL.Text = text;
+            byte[] bytes = image.ToJpegData();
+            Image x = (Bitmap)((new ImageConverter()).ConvertFrom(bytes));
+            pictureBox.Image = x;
+        }
+
+        private void LoadBTN_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog dialog = new OpenFileDialog();
+            if (dialog.ShowDialog() == DialogResult.OK)
+            {
+                video = Path.GetExtension(dialog.FileName) == ".mp4";
+                if (video)
+                {
+                    vFReader.Open(dialog.FileName);
+                    videoDetection();
+                }
+                else
+                {
+                    image = new Image<Bgr, byte>(dialog.FileName);
+                    if (pedestianDetection)
+                    {
+                        image2 = new CudaImage<Bgr, byte>(image);
+                        detectPersons();
+                    }
+                    else
+                    {
+                        yoloDection();
+                    }
+                }
+            }
+        }
+
+        private void pedestrianCheck_CheckedChanged(object sender, EventArgs e)
+        {
+            pedestianDetection = pedestrianCheck.Checked;
+            yoloBox.Checked = !pedestrianCheck.Checked;
+        }
+
+        private void yoloBox_CheckedChanged(object sender, EventArgs e)
+        {
+            yoloBox.Checked = yoloBox.Checked;
+            pedestrianCheck.Checked = !yoloBox.Checked;
+            pedestianDetection = !yoloBox.Checked;
+        }
+    }
+}

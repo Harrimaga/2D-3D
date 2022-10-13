4ca6d1fbb53282d68652fded6db6d3fe9b8db571
2D3D-Laptop <pjeromaster@gmail.com>
2022-10-13 16:10:41 +0200

    Init Upload

---
+++ objectherkenning/Program.cs
@@ -0,0 +1,17 @@
+﻿using Detection;
+using System;
+using System.Drawing;
+
+namespace objectherkenning
+{
+    internal class Program
+    {
+        static Bitmap b;
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            VideoStream v = new VideoStream();
+            Console.Read();
+        }
+    }
+}

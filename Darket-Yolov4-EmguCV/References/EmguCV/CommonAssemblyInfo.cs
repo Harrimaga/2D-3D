caf127976cc1c243caedf9ec589f7d1c9bd74056
Wai-Kei Law <w.k.law@students.uu.nl>
2022-10-15 16:02:21 +0200

    Object recognition with stream support

---
+++ Darket-Yolov4-EmguCV/References/EmguCV/CommonAssemblyInfo.cs
@@ -0,0 +1,11 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+[assembly: AssemblyVersion("4.4.0.4099")]
+[assembly: AssemblyFileVersion("4.4.0.4099")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Emgu Corporation")]
+[assembly: AssemblyProduct("Emgu.CV")]
+[assembly: AssemblyCopyright("Copyright Emgu Corporation 2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]

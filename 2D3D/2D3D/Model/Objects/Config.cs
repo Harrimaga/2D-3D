17ff361d758eab7312722d4ff354bab10f1560b8
Maninene <ielcaidi@icloud.com>
2022-10-20 13:16:08 +0200

    Beginning of some objects

---
+++ 2D3D/2D3D/Model/Objects/Config.cs
@@ -0,0 +1,12 @@
+﻿namespace _2D3D.Model
+{
+	public class Config
+	{
+		private ulong CID { get; }
+		private string name { get; }
+		//TO DO: membervariabelen voor het model en depth map maken (kan pas als implementatie duidelijk is)
+		public Config()
+		{
+		}
+	}
+}
---
+++ 2D3D/2D3D/Model/Objects/PersonObject.cs
@@ -0,0 +1,16 @@
+﻿namespace _2D3D
+{
+	public class PersonObject : DetectedObject
+	{
+		private float predictability;
+		//obj ID? of eigen soort ID?
+		//TO DO: position als membervariabele verwerken (kan pas als preprocessing af is)
+		//TO DO: datatype pixel navragen bij WK en DK
+		//TO DO: wat voor datatypes rotation en scale??
+
+		public PersonObject()
+		{
+			
+		}
+	}
+}

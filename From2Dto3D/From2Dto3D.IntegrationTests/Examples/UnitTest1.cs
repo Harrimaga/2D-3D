f2d825417edaec27fac9049e9515af3d6c24c0a7
Maninene <ielcaidi@icloud.com>
2022-10-27 16:03:06 +0200

    Testing: add testing environment Core application

---
+++ From2Dto3D/From2Dto3D.IntegrationTests/Examples/UnitTest1.cs
@@ -0,0 +1,15 @@
+namespace From2Dto3D.IntegrationTests.Examples;
+
+public class Tests
+{
+	[SetUp]
+	public void Setup()
+	{
+	}
+
+	[Test]
+	public void Test1()
+	{
+		Assert.Pass();
+	}
+}
---
+++ From2Dto3D/From2Dto3D.IntegrationTests/Usings.cs
@@ -0,0 +1,3 @@
+global using NUnit.Framework;
+global using From2Dto3D.Core;
+global using From2Dto3D.EmguCV;
